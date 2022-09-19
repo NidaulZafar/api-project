a28aeeea76d0b270a881eab7481a9bbd4fc19c66
N Zafar <n_zafar_007@hotmail.com>
2022-09-19 15:13:43 +0200

    weather and APOD done, image search started

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ window.addEventListener('load', () => {
             description.textContent = data.weather[0].description;
             temp.addEventListener('click', () => {
                 if (temperature === data.main.temp) {
-                    fahrenheit = (temperature * (9 / 5) + 32) ;
+                    let fahrenheit = (temperature * (9 / 5) + 32) ;
                     temp.innerHTML = `Current Temp is: ${Math.floor(fahrenheit)}<span>&#176;</span>F`;
                     temperature = 0;
                 } else {
@@ -53,6 +53,73 @@ window.addEventListener('load', () => {
         });
     }
 });
+
+
+const yesButton = document.querySelector('#yes');
+const noButton = document.querySelector('#no');
+const reaction = document.querySelector('.reaction');
+noButton.addEventListener('click', () => {
+    reaction.textContent = `You're Boring. Do you know how astronomers party?
+    They plan'et. Hahaha. Now Click the other button.`
+})
+const explanation = document.querySelector('.explanation')
+toggleHide('.explanation');
+yesButton.addEventListener('click', async () => {
+    reaction.textContent = `That's nice. Let me fetch some cool pictures from NASA`;
+    setTimeout(() => {
+        reaction.textContent = '';
+    }, 1500)
+    const apiNASA = 'TlcKEgRGq6uJcfvbGEGveueKIgs7WPsRnRvXvLrw';
+    const urlNASA = `https://api.nasa.gov/planetary/apod?api_key=${apiNASA}`
+    const response = await fetch(urlNASA);
+    const data = await response.json();
+    console.log(data);
+    const dataNASA = document.querySelector('.nasa-data');
+    const imgNASA = document.querySelector('#apod');
+    imgNASA.src = data.url;
+    toggleHide('.explanation')
+    const option = document.querySelector('#option');
+    const explanationPara = document.querySelector('.explanation-p')
+    option.addEventListener('click', () => {
+        explanationPara.textContent = option.checked ? data.explanation : 'Click here to learn more about this picture';
+        })   
+})
+
+// toggle the show explanation button
+function toggleHide(className) {
+    var element = document.querySelector(className);
+    element.classList.toggle("hidden");
+}
+
+
+
+const queryNasa = async () => {
+    const searchTerm = document.getElementById("search-input");
+    const query = searchTerm.value;
+    let urlNASA = `https://images-api.nasa.gov/search?q=${query}`;
+    const resp = await fetch(urlNASA);
+    const data = await resp.json()
+    console.log(data);
+const searchResultsNASA = document.querySelector('#nasa-search-results')
+    for (let i = 0; i < 10; i++) {
+        const li = document.createElement('li');
+        const a = document.createElement('a');
+        li.appendChild(a)
+        searchResultsNASA.appendChild(li);
+        a.innerHTML = JSON.stringify(`${data.collection.items[i]['data'][0].title}`);
+        a.href = `${data.collection.items[i]['links'][0].href}`;
+    }
+}
+
+
+const searchButton = document.querySelector('#search-btn');
+searchButton.addEventListener('click', queryNasa);
+
+
+
+
+
+
 
 
 
